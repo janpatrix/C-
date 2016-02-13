591e8bd478fc9d0a6e7d78bba5da0ebb1be074eb
Patrick <patrick.gross@niadminmacbook2.speedport_w_723v_1_39_000>
2016-02-13 12:00:12 +0100

    Initial version

---
+++ Tutorial1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int furlongs;
+    cout << "Please enter furlongs" << endl;
+    cin >> furlongs;
+    cout << furlongs << " furlongs are " << furlongs * 220 << " yards.";
+    return 0;
+}
+
